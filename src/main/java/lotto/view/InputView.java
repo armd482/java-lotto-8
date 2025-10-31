69e9784b9847d864036b158552425914dc57944f
armd482 <armd482@naver.com>
2025-10-31 14:09:16 +0900

    feat: inputView 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,24 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private final String PURCHASE_PRICE_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
+    private final String WINNING_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public String getPurchasePrice() {
+        System.out.println(PURCHASE_PRICE_INPUT_MESSAGE);
+        return Console.readLine().trim();
+    }
+
+    public String getWinningNumber() {
+        System.out.println(WINNING_NUMBER_INPUT_MESSAGE);
+        return Console.readLine().trim();
+    }
+
+    public String getBonusNumber() {
+        System.out.println(BONUS_NUMBER_INPUT_MESSAGE);
+        return Console.readLine().trim();
+    }
+}
