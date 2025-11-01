4f1d644c8799aebfac37315bd4184a57ba63d752
armd482 <armd482@naver.com>
2025-11-01 21:47:46 +0900

    test: 보너스 번호 모델 테스트

---
+++ src/test/java/lotto/model/BonusNumberTest.java
@@ -0,0 +1,29 @@
+package lotto.model;
+
+import lotto.constant.ErrorMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class BonusNumberTest {
+    @Test
+    @DisplayName("보너스번호 정상 초기화 테스트")
+    void BonusNumberInitTest() {
+        BonusNumber bonusNumber = new BonusNumber(1);
+        assertThat(bonusNumber.value()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("1보다 작은 보너스 번호 생성 오류 테스트")
+    void BonusNumberLowerValueInitTest() {
+        assertThatThrownBy(() -> new BonusNumber(-2)).isInstanceOf(IllegalArgumentException.class).hasMessage(
+                ErrorMessage.BONUS_NUMBER_RANGE_ERROR.getMessage());
+    }
+
+    @Test
+    @DisplayName("45보다  보너스 번호 생성 오류큰테스트")
+    void BonusNumberHigherValueInitTest() {
+        assertThatThrownBy(() -> new BonusNumber(100)).isInstanceOf(IllegalArgumentException.class).hasMessage(
+                ErrorMessage.BONUS_NUMBER_RANGE_ERROR.getMessage());
+    }
+}
