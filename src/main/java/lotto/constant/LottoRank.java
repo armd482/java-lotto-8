1c144ac222383b7cde9037a4a38f4a297ffd78e0
armd482 <armd482@naver.com>
2025-11-01 12:37:56 +0900

    feat: 로또 당첨 상수 구현

---
+++ src/main/java/lotto/constant/LottoRank.java
@@ -0,0 +1,47 @@
+package lotto.constant;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum LottoRank {
+    FIRST(6, false, 2000000000),
+    SECOND(5, true, 30000000),
+    THIRD(5, false, 1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3, false, 5000),
+    NONE(0, false, 0);
+
+    private final int matchCount;
+    private final boolean bonusMatch;
+    private final int prize;
+
+    LottoRank(int matchCount, boolean bonusMatch, int prize) {
+        this.matchCount = matchCount;
+        this.bonusMatch = bonusMatch;
+        this.prize = prize;
+    }
+
+    public static LottoRank getResult(int matchCount, boolean bonusMatch) {
+        List<LottoRank> matchCountRanks = Arrays.stream(LottoRank.values())
+                                                .filter(rank -> rank.matchCount == matchCount)
+                                                .toList();
+
+        if (matchCountRanks.size() == 1) {
+            return matchCountRanks.getFirst();
+        }
+
+        if(matchCountRanks.size() > 1) {
+            return matchCountRanks.stream()
+                                    .filter(rank -> rank.bonusMatch == bonusMatch)
+                                    .findFirst()
+                                    .orElse(NONE);
+        }
+
+        return NONE;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+}
